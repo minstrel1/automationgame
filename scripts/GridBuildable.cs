5f97a64e8b3af2dc45da740c44c3c0e5e46e857f
Vance <captainvman95@outlook.com>
2026-01-21 00:37:23 -0500

    i don't wanna break things yet but i will need to soon

---
+++ scripts/GridBuildable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel;
+using Godot;
+using Godot.Collections;
+using Godot.NativeInterop;
+
+[GlobalClass]
+[Tool]
+public partial class GridBuildable : GridEntity {
+
+}
